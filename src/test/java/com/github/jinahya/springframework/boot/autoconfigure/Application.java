f3df778a04a5f6cc07a7146aaf769fabfe63b8e6
Jin Kwon <onacit@gmail.com>
2019-05-10 22:02:44 +0900

    updated

--- src/test/java/com/github/jinahya/springframework/boot/autoconfigure/Application.java
+++
@@ -1,8 +0,0 @@
-package com.github.jinahya.springframework.boot.autoconfigure;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application {
-
-}

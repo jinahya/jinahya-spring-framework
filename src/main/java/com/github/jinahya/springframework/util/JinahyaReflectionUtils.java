71c370ba86692b102e7ac90ee05d1df9e5968bf0
Jin Kwon <onacit@wemakeprice.com>
2021-04-02 17:24:00 +0900

    updated

--- src/main/java/com/github/jinahya/springframework/util/JinahyaReflectionUtils.java
+++ src/main/java/com/github/jinahya/springframework/util/JinahyaReflectionUtils.java
@@ -1,17 +1,13 @@
 package com.github.jinahya.springframework.util;
 
-import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
-
-import static org.springframework.util.ReflectionUtils.findField;
-import static org.springframework.util.ReflectionUtils.getField;
-import static org.springframework.util.ReflectionUtils.makeAccessible;
-import static org.springframework.util.ReflectionUtils.setField;
 
 public abstract class JinahyaReflectionUtils {
 
@@ -28,56 +24,45 @@ public abstract class JinahyaReflectionUtils {
         ReflectionUtils.doWithFields(clazz, consumer::accept, predicate::test);
     }
 
-    /**
-     * Attempt to find a {@link java.lang.reflect.Field field} on the supplied {@link java.lang.Class Class} with the
-     * supplied name and/or {@link java.lang.Class type} and gets the field on the specified {@link java.lang.Object
-     * target object}.
-     *
-     * @param clazz  the class to introspect.
-     * @param name   the name of the field (may be null if type is specified).
-     * @param type   the type of the field (may be null if name is specified).
-     * @param target the target object on which to set the field (or {@code null} for a static field).
-     * @param <T>    field type parameter
-     * @return the field's current value.
-     * @see ReflectionUtils#findField(Class, String, Class)
-     * @see ReflectionUtils#getField(Field, Object)
-     */
-    @Nullable
-    @SuppressWarnings({"unchecked"})
-    public static <T> T findFieldAndGet(@NonNull final Class<?> clazz, @Nullable final String name,
-                                        @Nullable final Class<? extends T> type, @Nullable final Object target) {
-        final Field field = findField(clazz, name, type);
-        if (field == null) {
-            throw new RuntimeException("no field found for " + clazz + ", " + name + ", " + type);
-        } else if (!field.isAccessible()) {
-            makeAccessible(field);
-        }
-        return (T) getField(field, target);
+    public static void doWithLocalFields(final Class<?> clazz, final Consumer<? super Field> consumer) {
+        ReflectionUtils.doWithLocalFields(clazz, consumer::accept);
     }
 
-    /**
-     * Attempt to find a {@link java.lang.reflect.Field field} on the supplied {@link java.lang.Class Class} with the
-     * supplied name and/or {@link java.lang.Class type} and set the field on the specified {@link java.lang.Object
-     * target object} to the specified {@code value}.
-     *
-     * @param clazz  the class to introspect.
-     * @param name   the name of the field (may be null if type is specified).
-     * @param type   the type of the field (may be null if name is specified).
-     * @param target the target object on which to set the field (or {@code null} for a static field).
-     * @param value  the value to set (may be {@code null}).
-     * @param <T>    field type parameter
-     * @see ReflectionUtils#findField(Class, String, Class)
-     * @see ReflectionUtils#setField(Field, Object, Object)
-     */
-    public static <T> void findFieldAndSet(@NonNull final Class<?> clazz, @Nullable final String name,
-                                           @Nullable final Class<? super T> type, @Nullable final Object target,
-                                           @Nullable final T value) {
-        final Field field = findField(clazz, name, type);
-        if (field == null) {
-            throw new RuntimeException("no field found for " + clazz + ", " + name + ", " + type);
-        } else if (!field.isAccessible()) {
-            makeAccessible(field);
+    public static void doWithLocalMethods(final Class<?> clazz, final Consumer<? super Method> consumer) {
+        ReflectionUtils.doWithLocalMethods(clazz, consumer::accept);
+    }
+
+    public static void doWithMethods(final Class<?> clazz, final Consumer<? super Method> consumer) {
+        ReflectionUtils.doWithMethods(clazz, consumer::accept);
+    }
+
+    public static void doWithMethods(final Class<?> clazz, final Consumer<? super Method> consumer,
+                                     @Nullable final Predicate<? super Method> predicate) {
+        if (predicate == null) {
+            doWithMethods(clazz, consumer);
+            return;
         }
-        setField(field, target, value);
+        ReflectionUtils.doWithMethods(clazz, consumer::accept, predicate::test);
+    }
+
+    public static Optional<Field> findField(final Class<?> clazz, final String name) {
+        return Optional.ofNullable(ReflectionUtils.findField(clazz, name));
+    }
+
+    public static Optional<Field> findField(final Class<?> clazz, @Nullable final String name,
+                                            @Nullable final Class<?> type) {
+        return Optional.ofNullable(ReflectionUtils.findField(clazz, name, type));
+    }
+
+    public static Optional<Method> findMethod(final Class<?> clazz, final String name) {
+        return Optional.ofNullable(ReflectionUtils.findMethod(clazz, name));
+    }
+
+    public static Optional<Method> findMethod(final Class<?> clazz, final String name, final Class<?> type) {
+        return Optional.ofNullable(ReflectionUtils.findMethod(clazz, name, type));
+    }
+
+    public static Optional<Object> getField(final Field field, @Nullable final Object target) {
+        return Optional.ofNullable(ReflectionUtils.getField(field, target));
     }
 }
